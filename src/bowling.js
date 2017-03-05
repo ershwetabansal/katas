872bcea35f8a97136cb51b1754b11c61ca1d21d7
Shweta Bansal <ershwetabansal@gmail.com>
2017-03-05 19:30:13 +0000

    Make the tests green

--- src/bowling.js
+++ src/bowling.js
@@ -3,14 +3,24 @@ class Bowling {
     constructor() {
         this.points = 0;
         this.totalRolls = 0;
+        this.scoreBoard = [];
+        this.currentFrame = [];
     }
 
     roll(pins) {
-        if (this.totalFrames() >= 10) {
-            return false;
+        if (this.isGameOver()) {
+            throw new Error("Game over");
+        }
+        let currentPoints = pins;
+
+        if (this.isSpare()) {
+            currentPoints += pins;
         }
 
-        this.points = this.points + pins;
+        this.points = this.points + currentPoints;
+        this.resetCurrentFrame();
+        this.currentFrame.push(pins);
+        this.updateScoreBoard();
         this.totalRolls ++;
         return true;
     }
@@ -21,6 +31,30 @@ class Bowling {
 
     totalFrames() {
         return Math.floor(this.totalRolls / 2);
+    }
+
+    resetCurrentFrame() {
+        if (this.currentFrame.length === 2) {
+            this.currentFrame = [];
+        }
+    }
+
+    isGameOver() {
+        return this.totalFrames() >= 10;
+    }
+
+    updateScoreBoard() {
+        if (this.currentFrame.length === 2) {
+            this.scoreBoard.push(this.currentFrame);
+        }
+    }
+
+    isSpare() {
+        if (this.currentFrame.length === 2) {
+            return (this.currentFrame[0] + this.currentFrame[1]) === 10;
+        }
+
+        return false;
     }
 
 }
